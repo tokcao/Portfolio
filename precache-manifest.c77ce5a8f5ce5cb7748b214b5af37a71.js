795880f3d78ec55ef5bd3b07aa256d88280e62ec
Tianyu Cao <tianyu.cao@sjsu.edu>
2020-08-12 20:13:10 -0700

    Updates

---
+++ precache-manifest.c77ce5a8f5ce5cb7748b214b5af37a71.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46c348c166eddef297a8e043377b6da4",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "cf8db85ad5e059535d45",
+    "url": "/Portfolio/static/css/main.0911872f.chunk.css"
+  },
+  {
+    "revision": "d6d8739fe484833d3c7f",
+    "url": "/Portfolio/static/js/2.e3a7c448.chunk.js"
+  },
+  {
+    "revision": "cf89bcfdc86db2c261a743301fbeb514",
+    "url": "/Portfolio/static/js/2.e3a7c448.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf8db85ad5e059535d45",
+    "url": "/Portfolio/static/js/main.8a480d59.chunk.js"
+  },
+  {
+    "revision": "4b636f7edb3177af34d7",
+    "url": "/Portfolio/static/js/runtime-main.636edcf1.js"
+  },
+  {
+    "revision": "d56e7b7ae070e3cc1358c04f34faca70",
+    "url": "/Portfolio/static/media/avatar.d56e7b7a.jpeg"
+  },
+  {
+    "revision": "6c00cb9db242a8dc68ac85aadd53701b",
+    "url": "/Portfolio/static/media/mobile_shopping.6c00cb9d.png"
+  },
+  {
+    "revision": "1743a908fc75dec2d1838f8b4422ee8b",
+    "url": "/Portfolio/static/media/star_bucks.1743a908.jpg"
+  },
+  {
+    "revision": "0a37faf4ec787d750c580b13bf0df9a5",
+    "url": "/Portfolio/static/media/to_do_list.0a37faf4.jpg_fit=scale"
+  },
+  {
+    "revision": "b74643cbbe7d68c54554544ec50cf999",
+    "url": "/Portfolio/static/media/work.b74643cb.jpg"
+  }
+]);
